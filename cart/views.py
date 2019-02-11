a23ad10f7bf2995729fa668fc95cb6c0d1831cc6
chohanjoo <johanjoo@naver.com>
2019-02-11 14:56:57 +0900

    init

---
+++ cart/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def pay(request):
+    return render(request, 'cart/pay.html')
---
+++ menu/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Menu 
+# Register your models here.
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    pass
